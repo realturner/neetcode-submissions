d45488cc95a178bf101a76a12e5479104b5ecfab
realturner <realturner@gmail.com>
2026-05-31 22:29:34 +0800

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,48 @@
+// 21:47
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        const int n = edges.size();
+        vector<vector<pair<int,int>>> adj(n);
+        for (int i = 0; i < n; i++) {
+            auto &e = edges[i];
+            adj[e[0] - 1].push_back({e[1] - 1, i});
+            adj[e[1] - 1].push_back({e[0] - 1, i});
+        }
+        vector<bool> visited(n, false);
+        vector<pair<int,int>> curr; // (node, edgeIndex)
+        int index = -1;
+        function<bool(int,int)> go = [&](int u, int p) {
+            // cerr << u << ' ' << p << endl;
+            if (visited[u]) {
+                // if (index == -1) {
+                    // cout << "loop: ";
+                    // for (auto [u, idx] : curr) {
+                    //     cout << "(" << u << ", " << idx << ") ";
+                    // }
+                    // cout << endl;
+                    index = curr.back().second;
+                    for (int j = curr.size() - 2; j >= 0 && curr[j].first != u; j--) {
+                        index = max(index, curr[j].second);
+                    }
+                // }
+                return true;
+            }
+            visited[u] = true;
+            for (auto &[v, idx] : adj[u]) {
+                if (v != p) {
+                    curr.push_back({v, idx});
+                    if (go(v, u)) {
+                        return true;
+                    }
+                    curr.pop_back();
+                }
+            }
+            return false;
+        };
+        curr.push_back({0, -1});
+        go(0, -1);
+
+        return edges[index];
+    }
+};
