bca2f4190ebd410c78f8e1c29e164994cd9b7888
realturner <realturner@gmail.com>
2026-06-02 11:29:32 +0800

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.cpp
@@ -0,0 +1,25 @@
+// 11:13
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        reverse(matrix.begin(), matrix.end());
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+    }
+};
+
+/*
++-------------
+|         *
+|  *
+|          
+| 
+|     *
+|
++--------------
+
+*/
