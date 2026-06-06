51317d84a53d2533f42cba40f99415e09390958e
realturner <realturner@gmail.com>
2026-06-06 19:53:09 +0800

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.cpp
@@ -0,0 +1,16 @@
+// 19:48
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp;
+        for (int x : nums) {
+            auto it = lower_bound(dp.begin(), dp.end(), x);
+            if (it == dp.end()) {
+                dp.push_back(x);
+            } else {
+                *it = x;
+            }
+        }
+        return dp.size();
+    }
+};
