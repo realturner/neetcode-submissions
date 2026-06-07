47a80e8f9881141c0d387d6bfe93dbd1325077cc
realturner <realturner@gmail.com>
2026-06-07 15:08:17 +0800

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,20 @@
+// 14:48
+class Solution {
+public:
+    vector<int> partitionLabels(string &s) {
+        const int n = s.length();
+        unordered_map<char,int> right;
+        for (int i = 0; i < n; i++) {
+            right[s[i]] = i;
+        }
+        vector<int> ans;
+        for (int i = 0, p = 0, q = right[s[i]]; i < n; i++) {
+            q = max(q, right[s[i]]);
+            if (q == i) {
+                ans.push_back(q - p + 1);
+                p = q = i + 1;
+            }
+        }
+        return ans;
+    }
+};
