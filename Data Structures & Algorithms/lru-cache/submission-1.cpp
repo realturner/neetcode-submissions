de598eb2d22f590b57fa78f3518fa0d1fd078447
realturner <realturner@gmail.com>
2026-06-08 12:50:06 +0800

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.cpp
@@ -0,0 +1,39 @@
+// 12:26
+class LRUCache {
+    list<int> keys;
+    unordered_map<int,pair<int,list<int>::iterator>> cache;
+    const int limit;
+public:
+    LRUCache(int capacity): limit(capacity) {        
+    }
+    
+    int get(int key) {
+        auto it = cache.find(key);
+        if (it == cache.end()) {
+            return -1;
+        }
+        auto &[val, lit] = it->second;
+        keys.erase(lit);
+        keys.push_back(key);
+        lit = prev(keys.end());
+        return val;
+    }
+    
+    void put(int key, int value) {
+        auto it = cache.find(key);
+        if (it == cache.end()) {
+            keys.push_back(key);
+            cache[key] = {value, prev(keys.end())};
+            if (keys.size() > limit) {
+                cache.erase(keys.front());
+                keys.pop_front();
+            }
+        } else {
+            auto &[v, lit] = it->second;
+            v = value;
+            keys.erase(lit);
+            keys.push_back(key);
+            lit = prev(keys.end());
+        }
+    }
+};
