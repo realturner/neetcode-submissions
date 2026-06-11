de1e2a40ec839ce63eef624d99cd3ef360f81a2c
realturner <realturner@gmail.com>
2026-06-11 16:11:00 +0800

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.cpp
@@ -0,0 +1,45 @@
+class TrieNode {
+public:
+    TrieNode *nexts[26];
+    bool isEnd = false;
+};
+class WordDictionary {
+    TrieNode *root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string &word) {
+        auto u = root;
+        for (char ch : word) {
+            auto &next = u->nexts[ch - 'a'];
+            if (next == nullptr) {
+                next = new TrieNode();
+            }
+            u = next;
+        }
+        u->isEnd = true;
+    }
+    
+    bool search(string &word) {
+        const int n = word.length();
+        auto go = [&](this auto &&go, TrieNode *u, int i) -> bool {
+            while (u && i < n && word[i] != '.') {
+                u = u->nexts[word[i++] - 'a'];
+            }
+            if (u == nullptr) {
+                return false;
+            } else if (i == n) {
+                return u->isEnd;
+            }
+            for (auto v : u->nexts) {
+                if (v && go(v, i + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        return go(root, 0);
+    }
+};
