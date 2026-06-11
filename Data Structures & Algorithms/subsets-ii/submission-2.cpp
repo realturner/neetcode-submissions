e9ea02e5895ec25bc4f5907dddf0c902b214bf2c
realturner <realturner@gmail.com>
2026-06-11 15:58:49 +0800

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        const int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        auto go = [&](this auto &&go, int i) -> void {
+            ans.push_back(curr);
+            for (int j = i; j < n; j++) {
+                if (j > i && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                curr.push_back(nums[j]);
+                go(j + 1);
+                curr.pop_back();
+            }
+        };
+        go(0);
+        return ans;
+    }
+};
