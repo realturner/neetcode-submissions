a55df46bb6a579ec89cdd1963b1aab8984df7275
realturner <realturner@gmail.com>
2026-06-14 11:03:30 +0800

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.cpp
@@ -0,0 +1,26 @@
+// 10:45
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        const int m = matrix.size(), n = matrix[0].size();
+        vector<int> ans;
+        int w = n, h = m;
+        for (int i = 0, j = 0, di = 0, dj = 1; ans.size() < m * n; i += di, j += dj) {
+            // cerr << i << ' ' << j << endl;
+            ans.push_back(matrix[i][j]);
+            for (int k = --w; k > 0; k--) {
+                ans.push_back(matrix[i += di][j += dj]);
+                // cerr << i << ' ' << j << endl;
+            }
+            swap(di, dj);
+            dj = -dj;
+            for (int k = --h; k > 0; k--) {
+                ans.push_back(matrix[i += di][j += dj]);
+                // cerr << i << ' ' << j << endl;
+            }
+            swap(di, dj);
+            dj = -dj;
+        }
+        return ans;
+    }
+};
