24f50ce0c5e79f7c52349ea39a9a5f9abf508432
realturner <realturner@gmail.com>
2026-06-22 10:44:59 +0800

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        const int n = nums.size();
+        int ans = 0;
+        for (int i = 0 ; i < n; i++) {
+            ans += i + 1 - nums[i];
+        }
+        return ans;
+    }
+};
