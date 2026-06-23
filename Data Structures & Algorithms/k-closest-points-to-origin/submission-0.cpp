614c22a4d431c6655957d48810e955e4d57cdf29
realturner <realturner@gmail.com>
2026-06-23 10:53:07 +0800

    Add: k-closest-points-to-origin - submission-0

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-0.cpp
@@ -0,0 +1,11 @@
+// 10:49
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        nth_element(points.begin(), points.begin() + k, points.end(), [](vector<int> &a, vector<int> &b) {
+            return a[0] * a[0] + a[1] * a[1] < b[0] * b[0] + b[1] * b[1];
+        });
+        points.resize(k);
+        return points;
+    }
+};
